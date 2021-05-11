42d8ee0533954f02ef603ca698eb00ed616b7250
Yunsik-Choi <choiys0311@gmail.com>
2021-05-11 20:40:12 +0900

    apply rest-docs

--- src/test/java/com/Insurance/hm/employee/EmployeeControllerTest.java
+++ src/test/java/com/Insurance/hm/employee/EmployeeControllerTest.java
@@ -2,65 +2,127 @@ package com.Insurance.hm.employee;
 
 import com.Insurance.hm.employee.domain.Employee;
 import com.Insurance.hm.employee.domain.EmployeeRepository;
+import com.Insurance.hm.employee.domain.entity.Department;
 import com.Insurance.hm.employee.domain.entity.Role;
+import com.Insurance.hm.employee.dto.DetailEmployeeDto;
 import com.Insurance.hm.employee.dto.LoginEmployeeDto;
 import com.Insurance.hm.employee.dto.LoginInfoDto;
-import com.Insurance.hm.test.TestController;
+import com.Insurance.hm.util.ApiDocumentUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Before;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import javax.persistence.EntityManager;
 
-@AutoConfigureMockMvc
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.isA;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@ExtendWith({SpringExtension.class, RestDocumentationExtension.class})
+@WebMvcTest(EmployeeController.class)
 class EmployeeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
+    @MockBean
+    EntityManager em;
+    @MockBean
+    private EmployeeService employeeService;
+    @MockBean
     private EmployeeRepository employeeRepository;
     @Autowired
     private WebApplicationContext ctx;
-
-    ObjectMapper mapper = new ObjectMapper();
+    @Autowired
+    private ObjectMapper mapper;
 
     @BeforeEach
-    public void setup() {
+    public void setup(RestDocumentationContextProvider restDocumentation) {
         mockMvc = MockMvcBuilders.webAppContextSetup(ctx)
                 .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .apply(documentationConfiguration(restDocumentation))
                 .alwaysDo(print())
                 .build();
     }
 
     @Test
     void Employee로그인_테스트() throws Exception {
+        Employee employee = Employee.builder()
+                .name("name")
+                .login_id("abcd")
+                .password("1234")
+                .phone_number("010-5806-0321")
+                .email("choiys@naver.com")
+                .department(Department.개발)
+                .role(Role.과장)
+                .build();
 
-//        Employee employee = EmployeeTest.getEmployee();
-//        employeeRepository.save(employee);
-//
-//        LoginInfoDto loginInfoDto = new LoginInfoDto("abc","1234");
-//        MockHttpServletResponse model = mockMvc.perform(post("/api/employee/login/")
-//                .content(mapper.writeValueAsString(loginInfoDto))
-//                .contentType(MediaType.APPLICATION_JSON)
-//        ).andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
-//                .andReturn()
-//                .getResponse();
+        LoginInfoDto loginInfoDto = new LoginInfoDto();
+        loginInfoDto.setId("abcd");
+        loginInfoDto.setPassword("1234");
+
+        LoginEmployeeDto loginEmployeeDto = new LoginEmployeeDto(employee);
+
+        when(employeeService.login(loginInfoDto)).thenReturn(loginEmployeeDto);
+        ResultActions result = mockMvc.perform(RestDocumentationRequestBuilders.post("/api/employee/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(loginInfoDto))
+        );
+
+        result.andExpect(status().isOk())
+                .andDo(document("employee-login",
+                        ApiDocumentUtils.getDocumentRequest(),
+                        ApiDocumentUtils.getDocumentResponse(),
+                        requestFields(
+                                fieldWithPath("id").type(JsonFieldType.STRING).description("이름"),
+                                fieldWithPath("password").type(JsonFieldType.STRING).description("전화번호")
+                        ),
+                        responseFields(
+                                fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
+                                fieldWithPath("phoneNumber").type(JsonFieldType.STRING).description("전화번호"),
+                                fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+                                fieldWithPath("department").type(JsonFieldType.STRING).description("부서"),
+                                fieldWithPath("role").type(JsonFieldType.STRING).description("직급")
+                        )
+                        ));
 
     }
 
