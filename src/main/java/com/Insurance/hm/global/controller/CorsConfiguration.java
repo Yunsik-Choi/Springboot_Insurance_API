80c5ef3774fbb3e436113e380151add5824e0e7a
Yunsik-Choi <choiys0311@gmail.com>
2021-06-05 18:46:44 +0900

    cors configuration

--- src/main/java/com/Insurance/hm/global/controller/CorsConfiguration.java
+++ src/main/java/com/Insurance/hm/global/controller/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api")
+        registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000");
     }
 }
